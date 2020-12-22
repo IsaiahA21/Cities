ffd654467c62283261aa696d0b3e19fab9e20f70
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2020-12-21 22:57:57 -0700

    Allows the user to input as many cities and population and then display them on screen

---
+++ cities.cpp
@@ -0,0 +1,118 @@
+//  ENSF 337 Fall 2020 - Exercise B
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+struct  CityStruct {
+	double population;
+	char cityname[30];
+};
+void clearfile(const char* FileName);
+// prompts the user if they want to clear out the file before proceeding
+
+void writeInputFIle(const char* FileName, CityStruct);
+//Promises to write the city and population into the binary file
+
+void DisplayInputFile(const char* FileName);
+//display what is currently written on file 
+
+char userAnswer();
+
+int main() {
+	char FileName[] ="cities.bin";
+	CityStruct AddCity;
+	char inp;
+	
+	clearfile(FileName);
+	
+	do {
+	cout<<endl;
+	cout<< "Do you want to add Cities to the file (y for yes and n for no): ";
+	cin>>inp;
+	}while (inp != 'y' && inp != 'n' && inp != 'Y' && inp != 'N'); 
+	
+	while(inp == 'y' || inp == 'Y') {
+		cout << "Please enter the city name: ";
+		cin>>AddCity.cityname;
+		cout<<"Now, enter the population of the city: ";
+		cin>>AddCity.population;
+		writeInputFIle(FileName, AddCity);
+		
+		do {
+		cout<<endl;
+		cout<< "Do you want to add another(y for yes and n for no): ";
+		cin>>inp;
+		}while (inp != 'y' && inp != 'n' && inp != 'Y' && inp != 'N'); 
+		
+	}
+	
+	do {
+	cout<<endl;
+	cout<<"Do you want to read the current file y for yes and n for no): ";
+    cin>>inp;
+	}while (inp != 'y' && inp != 'n' && inp != 'Y' && inp != 'N');
+	
+	if (inp == 'Y' || inp == 'y') {
+	  DisplayInputFile(FileName);
+	}
+	return 0;
+}
+
+void clearfile (const char* FileName) {
+	char cle;
+	do {
+	cout<<endl;
+	cout << "Do you want to clearout previous input writtten to file(y for yes  and n for no): ";
+	cin>>cle;
+    }while (cle != 'y' && cle != 'n' && cle != 'Y' && cle != 'N');
+	if (cle == 'n'|| cle == 'N') {
+		return;
+	}
+	ofstream out (FileName, ios::out | ios::binary);
+	if (out.fail()){
+		cout<< "Couldn't properly open file"<<endl;
+		//exit(1);
+	} 
+	out.clear();
+	out.close();
+	cout <<endl;
+	cout<< "File cleared"<<endl;
+	
+
+}
+void writeInputFIle(const char* FileName, const CityStruct InputCity) {
+	ofstream outfile (FileName, ios::out | ios::binary| ios::app);
+	if (outfile.fail()){
+		cout<< "Couldn't properly open file"<<endl;
+		exit(1);
+	}
+    outfile.write((char*)&InputCity,sizeof(CityStruct));
+    outfile.close();	
+}
+
+void DisplayInputFile(const char* FileName){
+	
+		ifstream Infile(FileName, ios::in|ios::binary);
+	 if(!Infile) {
+		 cout<<"Error, couldn't open"<<endl;
+		 exit(1);
+	 }
+	Infile.seekg(0,ios::end);
+	int numberOfRecords = Infile.tellg()/sizeof(CityStruct);
+	CityStruct ReadingCities[numberOfRecords];
+	
+	Infile.seekg(0,ios::beg);
+	Infile.read((char*)ReadingCities, sizeof(CityStruct)*numberOfRecords);
+	Infile.close();
+	
+	for (int i =0; i< numberOfRecords; i++) {
+		cout<< "City: " << ReadingCities[i].cityname<<endl;
+        cout <<"Population "<< ReadingCities[i].population<<endl<<endl;
+	}
+}
+
