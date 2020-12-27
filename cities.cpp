93870e1988da0cf66fc4d2dc62d47eecfb285ab7
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2020-12-26 18:11:20 -0700

    Added a function to search file for a specific city

--- cities.cpp
+++ cities.cpp
@@ -4,11 +4,13 @@
 #include <stdio.h>
 #include <string>
 using namespace std;
+#include <cstring>
 
 struct  CityStruct {
 	double population;
 	char cityname[30];
 };
+
 void clearfile(const char* FileName);
 // prompts the user if they want to clear out the file before proceeding
 
@@ -18,7 +20,18 @@ void writeInputFIle(const char* FileName, CityStruct);
 void DisplayInputFile(const char* FileName);
 //display what is currently written on file 
 
-char userAnswer();
+//Prompts user for the cities they want to add
+void GetCity_n_pop(const char* FileName,CityStruct);
+
+//Search for city by name 
+//Returns 0 if no city by that name was found
+// else displays the city with the same name 
+// and returns the number of cities found
+void Searchbyname(const char* FileName);
+
+/*char userAnswer();*/
+
+
 
 int main() {
 	char FileName[] ="cities.bin";
@@ -27,36 +40,39 @@ int main() {
 	
 	clearfile(FileName);
 	
+    // Adding cities to the cit
 	do {
 	cout<<endl;
-	cout<< "Do you want to add Cities to the file (y for yes and n for no): ";
+	cout<< "Do you want to add Cities to the file? (y for yes and n for no): ";
 	cin>>inp;
 	}while (inp != 'y' && inp != 'n' && inp != 'Y' && inp != 'N'); 
-	
-	while(inp == 'y' || inp == 'Y') {
-		cout << "Please enter the city name: ";
-		cin>>AddCity.cityname;
-		cout<<"Now, enter the population of the city: ";
-		cin>>AddCity.population;
-		writeInputFIle(FileName, AddCity);
-		
-		do {
-		cout<<endl;
-		cout<< "Do you want to add another(y for yes and n for no): ";
-		cin>>inp;
-		}while (inp != 'y' && inp != 'n' && inp != 'Y' && inp != 'N'); 
-		
+
+	if (inp == 'Y' || inp == 'y') {
+	 	GetCity_n_pop( FileName,AddCity);
 	}
-	
+
+	//searching by city name 
 	do {
 	cout<<endl;
-	cout<<"Do you want to read the current file y for yes and n for no): ";
+	cout<<"Do you want to search for a specific city? (Enter yes and n for no): ";
+    cin>>inp;
+	}while (inp != 'y' && inp != 'n' && inp != 'Y' && inp != 'N');
+	
+	if (inp == 'Y' || inp == 'y') {
+	  Searchbyname(FileName);
+	}
+
+    //display the file
+	do {
+	cout<<endl;
+	cout<<"Do you want to display the file? (y for yes and n for no): ";
     cin>>inp;
 	}while (inp != 'y' && inp != 'n' && inp != 'Y' && inp != 'N');
 	
 	if (inp == 'Y' || inp == 'y') {
 	  DisplayInputFile(FileName);
 	}
+
 	return 0;
 }
 
@@ -82,6 +98,24 @@ void clearfile (const char* FileName) {
 	
 
 }
+
+void GetCity_n_pop(const char* FileName, CityStruct AddCity) {
+	char userInput;
+	do {
+
+		cout << "Please enter the city name: ";
+		cin>>AddCity.cityname;
+		cout<<"Now, enter the population of the city: ";
+		cin>>AddCity.population;
+		writeInputFIle(FileName, AddCity);
+		
+
+		cout<<endl;
+		cout<< "Do you want to add another(y for yes and n for no): ";
+		cin>>userInput;
+	}while(userInput == 'y' || userInput == 'Y');
+		
+}
 void writeInputFIle(const char* FileName, const CityStruct InputCity) {
 	ofstream outfile (FileName, ios::out | ios::binary| ios::app);
 	if (outfile.fail()){
@@ -92,19 +126,72 @@ void writeInputFIle(const char* FileName, const CityStruct InputCity) {
     outfile.close();	
 }
 
+void Searchbyname(const char* FileName){
+	char targetCity[30];
+	char keepSearching;
+
+	ifstream checkStream(FileName, ios::in| ios::binary);
+	if(!checkStream ) {
+		cout<<"Sorry couldn't open file for searching"<<endl;
+		return;
+	}
+
+	// goes to the end of file 
+	checkStream.seekg(0,ios::end);
+
+	//returns the end of file then divides by the size of CityStruct
+	int numberOfRecords = checkStream.tellg()/sizeof(CityStruct);
+
+	// Creates an array of Structs
+	CityStruct SearchbyCities[numberOfRecords];
+	
+	//returns to the beginning of file
+	checkStream.seekg(0,ios::beg);
+	checkStream.read((char*)SearchbyCities, sizeof(CityStruct)*numberOfRecords);
+	checkStream.close();
+   do 
+   {
+	 cout<<"Enter the name of the city you want to find: ";
+	 cin>>targetCity;
+	 int matches=0;
+	 for (int i =0; i< numberOfRecords; i++) {
+		if (strcmp(targetCity,SearchbyCities[i].cityname)==0) {
+		  matches++;
+		}
+	 }
+	 cout<<"There are "<< matches << " cities that matched the target:"<<endl;
+	 if (matches>0)
+	 {
+	  cout<<"Matched city: "<<endl;
+	  for (int i =0; i< numberOfRecords; i++) {
+		if (strcmp(targetCity,SearchbyCities[i].cityname)==0) {
+		  cout<< "City: " << SearchbyCities[i].cityname<<endl;
+		  cout <<"Population "<< SearchbyCities[i].population<<endl<<endl;
+		}
+	  }
+	}
+	 cout<<endl;
+	 cout<< "Do you want to search for another city? (y for yes and n for no): ";
+	 cin>>keepSearching;
+    }while (keepSearching == 'y' ||  keepSearching == 'Y'); 
+}
+
 void DisplayInputFile(const char* FileName){
 	
 		ifstream Infile(FileName, ios::in|ios::binary);
 	 if(!Infile) {
 		 cout<<"Error, couldn't open"<<endl;
 		 exit(1);
-	 }
+}
 	// goes to the end of file 
 	Infile.seekg(0,ios::end);
+
 	//returns the end of file then divides by the size of CityStruct
 	int numberOfRecords = Infile.tellg()/sizeof(CityStruct);
+
 	// Creates an array of Structs
 	CityStruct ReadingCities[numberOfRecords];
+
 	//returns to the beginning of file
 	Infile.seekg(0,ios::beg);
 	Infile.read((char*)ReadingCities, sizeof(CityStruct)*numberOfRecords);
@@ -115,4 +202,5 @@ void DisplayInputFile(const char* FileName){
         cout <<"Population "<< ReadingCities[i].population<<endl<<endl;
 	}
 }
+
 
